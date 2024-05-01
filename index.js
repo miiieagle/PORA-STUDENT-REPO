ed0a4e771b07ba23eff2677ced63a5d1eb1f3e1d
miiieagle <miieagle06@gmail.com>
2024-05-01 11:52:48 +0300

    listening to the port

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-
-const dev = (par1,par2) => {
-   console.log({sum:par1+par2})
+const express = require("express");
+require("dotenv").config();
+const app = express()
+//secret key
+console.log(process.env.Deborah_key)
+//listening to the port
+const port = process.env.PORT
+app.listen(port, () => {
+   console.log("listening on port:" + port)
+})
+//routing
+app.get("/student", (req, res, next) => {
+const studentDetails ={
+   name: "Jude Adeyemi",
+   age: 16,
+   class: 2023,
+}
+res.send(studentDetails)
+})
+const dev = (display) => {
    console.log("Adeyemi Deborah Doyinsola")
-   
    }
-   
-   dev(2, 3)
+   dev()
       
